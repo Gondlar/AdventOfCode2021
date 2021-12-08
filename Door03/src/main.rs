8c763b6a4796ddf10cc426e6863a7d4f0da392c7
Patrick Hansert <gondlar@gmx.de>
2021-12-08 23:38:02 +0100

    Door 03b

--- Door03/src/main.rs
+++ Door03/src/main.rs
@@ -61,7 +61,7 @@ impl Diagnostic {
         Diagnostic{total: 0, counts: vec!()}
     }
 
-    fn add(&mut self, bits: Bits) {
+    fn add(&mut self, bits: &Bits) {
         let len = bits.len();
         if len > self.counts.len() {
             self.counts.resize(len, 0);
@@ -89,20 +89,50 @@ impl Diagnostic {
     }
 }
 
+fn oxy_rating(ones : usize, zeros : usize) -> bool {
+    return ones >= zeros
+}
+
+fn co2_rating(ones : usize, zeros : usize) -> bool {
+    return ones < zeros
+}
+
+fn ls_rating(data : Vec<&Bits>, digit : usize, pred: fn(usize, usize) -> bool) -> &Bits {
+    let remaining = data.len();
+    if remaining == 0 {
+        panic!("No Bitstrings left. This shouldn't happen");
+    } else if remaining == 1 {
+        return data[0];
+    }
+    if data[0].len() <= digit {
+        panic!("Multiple Bitstrings left when we're out of bits. This shouldn't happen.");
+    }
+    let ones = data.iter()
+                         .filter(|bits| bits[digit])
+                         .count();
+    let target_value = pred(ones, remaining - ones);
+    return ls_rating(data.into_iter()
+                              .filter(|bits| bits[digit] == target_value)
+                              .collect(),
+                     digit+1, pred);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-    //let use_aim : bool = args[2].parse::<bool>().unwrap_or(false);
 
     let contents = fs::read_to_string(filename)
         .expect("Something went terribly wrong while reading the file!");
     let mut diag : Diagnostic = Diagnostic::new();
-    contents.split("\n")
-            .map(|line| line.parse::<Bits>())
-            .filter(|line| line.is_ok())
-            .for_each(|line| {
-                let bits = line.unwrap();
-                diag.add(bits);
-            });
+    let bits : Vec<Bits> = contents.split("\n")
+                       .map(|line| line.parse::<Bits>())
+                       .filter(|line| line.is_ok())
+                       .map(|line| line.unwrap())
+                       .filter(|line| line.len() > 0)
+                       .collect();
+    bits.iter().for_each(|entry| diag.add(entry));
     println!("{}", u32::from(&diag.epsilon())* u32::from(&diag.gamma()));
+    let oxy = u32::from(ls_rating(bits.iter().collect(), 0, oxy_rating));
+    let co2 = u32::from(ls_rating(bits.iter().collect(), 0, co2_rating));
+    println!("Oxy: {} / CO2: {}\n{}", oxy, co2, oxy*co2);
 }
